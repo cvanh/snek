03ce561cb7139b0ca2de1654a2be635223d5f5cc
constantijn van hartesveldt <constantijn@vanhartesveldt.nl>
2021-03-02 11:55:56 +0100

    shit this is going better then expexted

--- client/assets/main.js
+++ client/assets/main.js
@@ -11,23 +11,34 @@ socket.on('message', text => {
 document.querySelector('button').onclick = () => {
 
     const text = document.querySelector('input').value;
-    socket.emit('message', text)
+    socket.emit('message', text) // text is bijv 'hoi'
     
 }
-
+for (var m = 3, n = 1E3 / m, p = .5, q = 0, r = !1, t = 0; 30 > t; t++)
+    for (var u = 0; 30 > u; u++) {
+        var grid = document.createElement("div");
+        grid.setAttribute("id", u + "-" + t);
+        grid.setAttribute("class", "cell");
+        grid.style.width = "2.5vmin";
+        grid.style.height = "2.5vmin";
+        board.appendChild(grid)
+    }
 document.addEventListener("keydown", function logKey(e) {
     var key = e.key;
     if (key == "ArrowUp") {
         console.log("arrow up");
-        socket.emit('arrow up', key)
+        socket.emit('message', key)
     }
     if (key == "ArrowDown") {
         console.log("arrow down");
+        socket.emit('message', key)
     }
     if (key == "ArrowLeft") {
         console.log("arrow left");
+        socket.emit('message', key)
     }
     if (key == "ArrowRight") {
         console.log("arrow right");
+        socket.emit('message', key)
     }
 });
